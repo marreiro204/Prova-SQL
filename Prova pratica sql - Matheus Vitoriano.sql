d4d76fd2db7af83f36d80befdaa01e36f6fd68ea
vitorianno204 <165702810+marreiro204@users.noreply.github.com>
2024-11-06 14:50:06 -0300

    Update Prova pratica sql - Matheus Vitoriano.sql

--- Prova pratica sql - Matheus Vitoriano.sql
+++ Prova pratica sql - Matheus Vitoriano.sql
@@ -1,8 +1,8 @@
 CREATE DATABASE PetShop -- DATABASE PARA UM EMPRESA DE PETSHOP
 
----- PARTE 1 CRIA«√O DAS TABELAS DO BANCO DE DADOS ----
+---- PARTE 1 CRIA√á√ÉO DAS TABELAS DO BANCO DE DADOS ----
 USE PetShop
--- CRIA«√O DA TABELA DE VETERINARIOS DA EMPRESA
+-- CRIA√á√ÉO DA TABELA DE VETERINARIOS DA EMPRESA
 CREATE TABLE Veterinarios(
 ID_Fun VARCHAR(20),
 nome VARCHAR (50),
@@ -11,7 +11,7 @@ cpf VARCHAR (14),
 turno VARCHAR (6),
 sexo VARCHAR (2)
 )
--- VALORES E INFORMA«’ES INSERIDOS NA TABELA VETERINARIOS
+-- VALORES E INFORMA√á√ïES INSERIDOS NA TABELA VETERINARIOS
 INSERT INTO  Veterinarios (ID_Fun, nome, crm, cpf, turno,sexo) VALUES
 ('1', 'Cleiton Xavier', '12345678910', '23414165235', 'Manha', 'M'),
 ('2', 'Dandara Santos', '43645678910', '44514165235', 'Tarde', 'F'),
@@ -23,7 +23,7 @@ SELECT* FROM Veterinarios
 DROP TABLE Veterinarios
 
 DROP TABLE Veterinarios
--- CRIA«√O DA TABELA DE TOSADORES DA EMPRESA
+-- CRIA√á√ÉO DA TABELA DE TOSADORES DA EMPRESA
 CREATE TABLE Tosadores(
 ID_Fun VARCHAR(20),
 nome VARCHAR (50),
@@ -33,7 +33,7 @@ sexo VARCHAR (2)
 )
 DROP TABLE Tosadores
 SELECT*FROM Tosadores
--- VALORES E INFORMA«’ES INSERIDOS NA TABELA TOSADORES
+-- VALORES E INFORMA√á√ïES INSERIDOS NA TABELA TOSADORES
 INSERT INTO Tosadores (ID_Fun, nome,cpf, turno,sexo) VALUES
 ('10', 'Charles Xavier', '39824165235', 'Manha', 'M'),
 ('20', 'Luana Martins', '27414208435', 'Tarde', 'F'),
@@ -41,7 +41,7 @@ INSERT INTO Tosadores (ID_Fun, nome,cpf, turno,sexo) VALUES
 ('40', 'Lucas Marreiro', '70414164535', 'Noite', 'M'),
 ('50', 'Leticia Bufoni', '98614208235', 'Noite', 'F')
 
--- CRIA«√O DA TABELA DE FUNCIONARIOS DA EMPRESA
+-- CRIA√á√ÉO DA TABELA DE FUNCIONARIOS DA EMPRESA
 CREATE TABLE Funcionarios(
 ID_Fun VARCHAR(20),
 nome VARCHAR (50),
@@ -51,7 +51,7 @@ sexo VARCHAR (2),
 salario INT
 )
 select*from Funcionarios
--- VALORES E INFORMA«’ES INSERIDOS NA TABELA FUNCIONARIOS
+-- VALORES E INFORMA√á√ïES INSERIDOS NA TABELA FUNCIONARIOS
 INSERT INTO Funcionarios(ID_Fun, nome,cpf, turno,sexo, salario) VALUES
 ('1', 'Cleiton Xavier','23414165235', 'Manha', 'M', 5000),
 ('2', 'Dandara Santos','44514165235', 'Tarde', 'F', 4500),
@@ -65,50 +65,50 @@ INSERT INTO Funcionarios(ID_Fun, nome,cpf, turno,sexo, salario) VALUES
 ('50', 'Leticia Bufoni', '98614208235', 'Noite', 'F', 3000)
 DROP TABLE Funcionarios
 
--- CRIA«√O DA TABELA DE ANIMAIS CADASTRADOS DA EMPRESA
+-- CRIA√á√ÉO DA TABELA DE ANIMAIS CADASTRADOS DA EMPRESA
 CREATE TABLE AnimaisCadastrados(
 ID_Anm VARCHAR(20),
 ID_Cliente VARCHAR (20),
 nome VARCHAR (50),
 Tipo VARCHAR (25),
-RaÁa VARCHAR (25),
+Ra√ßa VARCHAR (25),
 sexo VARCHAR (2)
 )
 select*from AnimaisCadastrados
--- VALORES E INFORMA«’ES INSERIDOS NA TABELA DE ANIMAIS CADASTRADOS
-INSERT INTO AnimaisCadastrados (ID_Anm, ID_Cliente, nome, Tipo, RaÁa, sexo) VALUES
+-- VALORES E INFORMA√á√ïES INSERIDOS NA TABELA DE ANIMAIS CADASTRADOS
+INSERT INTO AnimaisCadastrados (ID_Anm, ID_Cliente, nome, Tipo, Ra√ßa, sexo) VALUES
 ('100', '1', 'Bob', 'Cachorro', 'Golden Retrivier', 'M'),
 ('101', '2', 'Olivia', 'Cachorro', 'Pitbull', 'F'),
 ('102', '1', 'Noha', 'Gato', 'Feilicious ray', 'M'),
 ('103', '3', 'Hulk', 'Cachorro', 'Labrador', 'M'),
-('104', '2', 'Napole„o', 'Cachorro', 'Dalm·ta', 'M')
+('104', '2', 'Napole√£o', 'Cachorro', 'Dalm√°ta', 'M')
 
--- CRIA«√O DA TABELA DE CLIENTES DA EMPRESA
+-- CRIA√á√ÉO DA TABELA DE CLIENTES DA EMPRESA
 CREATE TABLE Clientes(
 ID_Cliente VARCHAR (20),
 cpf VARCHAR (14),
 nome VARCHAR (50),
 Qtd_Animais_Cadastrados INT,
-EndereÁo VARCHAR (200),
+Endere√ßo VARCHAR (200),
 sexo VARCHAR (2)
 )
 select*from Clientes
--- VALORES E INFORMA«’ES INSERIDOS NA TABELA DE CLIENTES
-INSERT INTO Clientes (ID_Cliente, cpf, nome, Qtd_Animais_Cadastrados, EndereÁo,sexo) VALUES
+-- VALORES E INFORMA√á√ïES INSERIDOS NA TABELA DE CLIENTES
+INSERT INTO Clientes (ID_Cliente, cpf, nome, Qtd_Animais_Cadastrados, Endere√ßo,sexo) VALUES
 ('1', '32726828615', 'Claudia Santos', 2, 'Rua Coelho Lousada 144', 'F'),
 ('2', '86426828705', 'Romeu Garcia', 2, 'Rua Barbosa de Freitas 2004', 'M'),
 ('3', '47526828305', 'Caike Neves', 1, 'Rua Parvati 374', 'M')
 
----- PARTE 2 IMPLEMENTA«√O DE FUN«’ES  ----
+---- PARTE 2 IMPLEMENTA√á√ÉO DE FUN√á√ïES  ----
 
 --- VIEWS ---
 
---- CRIANDO UMA VIEW PARA EU CONSEGUIR CONSULTAR O DONO DO ANIMAL PELO ID DO CLIENTE QUE EST¡ PRESENTA NAS TABELAS CLIENTES E ANIMAIS CADASTRADOS
+--- CRIANDO UMA VIEW PARA EU CONSEGUIR CONSULTAR O DONO DO ANIMAL PELO ID DO CLIENTE QUE EST√Å PRESENTA NAS TABELAS CLIENTES E ANIMAIS CADASTRADOS
 CREATE OR ALTER VIEW vw_animais_clientes AS
 SELECT 
 AC.nome AS 'Nome do animal',
 AC.Tipo,
-AC.RaÁa,
+AC.Ra√ßa,
 Clientes.nome AS 'Nome do dono'
 FROM AnimaisCadastrados AS AC
 INNER JOIN Clientes ON Clientes.ID_Cliente = AC.ID_Cliente
@@ -117,7 +117,7 @@ SELECT * FROM vw_animais_clientes
 
 ---  Subqueries ---
 
---- CRIANDO UMA SUBQUERIE PARA SABER OS FUNCION¡RIOS MASCULINOS QUE TRABALHAM NO TURNO DA NOITE
+--- CRIANDO UMA SUBQUERIE PARA SABER OS FUNCION√ÅRIOS MASCULINOS QUE TRABALHAM NO TURNO DA NOITE
 
 SELECT *
 FROM Funcionarios 
@@ -126,24 +126,30 @@ SELECT turno
 FROM Funcionarios 
 WHERE turno = 'NOITE' AND sexo = 'M')
 
---- CRIANDO UMA CTE PARA------
+--- CRIANDO UMA CTE PARA  SABER FUNCION√ÅRIOS DO TIPO VETREIN√ÅRIO------
 
-WITH Funcion·riosMasculinoVet AS (
-SELECT 
-Funcionarios.ID_Fun,
-Funcionarios.nome,
-Funcionarios.turno,
-Funcionarios.sexo
-FROM Funcionarios
-INNER JOIN Veterinarios ON Veterinarios.ID_vet = Funcionarios.ID_Fun
-
+WITH FuncionariosVet AS (
+    SELECT 
+        Funcionarios.ID_Fun,
+        Funcionarios.nome,
+        Funcionarios.turno,
+        Funcionarios.sexo
+    FROM Funcionarios
+    INNER JOIN Veterinarios ON Veterinarios.ID_vet = Funcionarios.ID_Fun
 )
+SELECT * 
+FROM FuncionariosVet;
+
+
+SELECT * 
+FROM FuncionariosMasculinoVet;
+
 
 ---- Window Functions ----
---- FUN«√O PARA SABER O SALARIO DOS VETREIN¡RIOS
+--- FUN√á√ÉO PARA SABER O SALARIO DOS VETREIN√ÅRIOS
 SELECT 
 veterinarios.nome,
-SUM(salario) AS '  SALARIO DOS VETERIN¡RIOS'
+SUM(salario) AS '  SALARIO DOS VETERIN√ÅRIOS'
 FROM Funcionarios
 INNER JOIN veterinarios ON veterinarios.ID_Fun = Funcionarios.ID_Fun
 GROUP BY veterinarios.nome
@@ -151,7 +157,7 @@ GROUP BY veterinarios.nome
 
 
 ------------ FUNCTIONS -------------
---- FUN«√O CRIADA PARA SABER O NOME, O CPF, TURNO, SEXO E SAL¡RIO DO FUNCION¡RIO PELO ID, EXEMPLO: SE VOC  QUER SABER DO FUNCION¡RIO COM ID 1 ELE VAI TRAZER ESSAS INFORMA«’ES SOMENTE DELE
+--- FUN√á√ÉO CRIADA PARA SABER O NOME, O CPF, TURNO, SEXO E SAL√ÅRIO DO FUNCION√ÅRIO PELO ID, EXEMPLO: SE VOC√ä QUER SABER DO FUNCION√ÅRIO COM ID 1 ELE VAI TRAZER ESSAS INFORMA√á√ïES SOMENTE DELE
 CREATE FUNCTION ConsultasPorFuncionario (@ID_Fun INT)
 RETURNS @TabelaConsulta TABLE (
 ID_Fun VARCHAR(20),
@@ -177,13 +183,13 @@ SELECT * FROM dbo.ConsultasPorFuncionario(1)
 
 
 -------- TRIGGERS -------------
------- CRIEI UM TRIGGER COM UMA REGRA PARA QUE NINGU…M DA EMPRESA DELETE QUALQUER CLIENTE QUE SEJA
+------ CRIEI UM TRIGGER COM UMA REGRA PARA QUE NINGU√âM DA EMPRESA DELETE QUALQUER CLIENTE QUE SEJA
 CREATE OR ALTER TRIGGER Deletar_Cliente
 ON Clientes
 INSTEAD OF DELETE
 AS
 BEGIN
-	PRINT 'Esses dados n„o podem ser deletados'
+	PRINT 'Esses dados n√£o podem ser deletados'
 END
 GO
 
@@ -196,15 +202,33 @@ CREATE PROCEDURE sp_inserir_clientes
 @cpf VARCHAR (14),
 @nome VARCHAR (50),
 @Qtd_Animais_Cadastrados INT,
-@EndereÁo VARCHAR (200),
+@Endere√ßo VARCHAR (200),
 @sexo VARCHAR (2)
 AS
 BEGIN
-    INSERT INTO Clientes ( cpf, nome, Qtd_Animais_Cadastrados, EndereÁo,sexo)
-    VALUES ( @cpf, @nome,@Qtd_Animais_Cadastrados,@EndereÁo,@sexo)
+    INSERT INTO Clientes ( cpf, nome, Qtd_Animais_Cadastrados, Endere√ßo,sexo)
+    VALUES ( @cpf, @nome,@Qtd_Animais_Cadastrados,@Endere√ßo,@sexo)
 
 	END
 	EXEC sp_inserir_clientes 44512378923, mauro, 1, ruaboechat145, M
 
 	select*from Clientes
+	------------- LOOP ----------
+	---- ESSE LOOP 
+	-- Loop para atualizar o turno de todos os funcion√°rios que est√£o no turno da noite
+	DECLARE @NovoTurno VARCHAR(7) = 'Tarde';
+DECLARE @TurnoAntigo VARCHAR(7) = 'Noite';  
+DECLARE @Id_Fun INT;
+
+
+WHILE EXISTS (SELECT 1 FROM Tosadores WHERE turno = @TurnoAntigo)
+BEGIN 
+    SELECT TOP 1 @Id_Fun = ID_Fun
+    FROM Tosadores
+    WHERE turno = @TurnoAntigo;
+
+    UPDATE Tosadores
+    SET turno = @NovoTurno
+    WHERE ID_Fun = @Id_Fun; 
+END
 
